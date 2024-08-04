fc2ea8ff7b4e838b0501a6e9fc99e7e48c09b71e
JaeWon Heo <hy87412@naver.com>
2024-08-04 12:23:29 +0900

    add check functions

--- main.py
+++ main.py
@@ -22,3 +22,73 @@ for row in range(9):
         if sudokuboard[row][col] == '':
             emptypos.append([row,col])
 
+#행(row)검사 함수
+def checkrow(selectrow):
+    inlist = []
+    for col in range(9):
+        if sudokuboard[selectrow][col] != ' ':
+            inlist.append(sudokuboard[selectrow][col])
+    outlist = []
+    for i in range(1,10,1):
+        if i not in inlist:
+            outlist.append(i)
+    return outlist
+
+#열(col)검사 함수
+def checkcol(selectcol):
+    inlist = []
+    for row in range(9):
+        if sudokuboard[row][selectcol] != ' ':
+            inlist.append(sudokuboard[row][selectcol])
+    outlist = []
+    for i in range(1,10,1):
+        if i not in inlist:
+            outlist.append(i)
+    return outlist
+
+#구역(area)검사 함수
+def checkarea(selectarea):
+    if selectarea == 0:
+        movex = -3
+        movey = -3
+    elif selectarea == 1:
+        movex = 0
+        movey = -3
+    elif selectarea == 2:
+        movex = 3
+        movey = -3
+    elif selectarea == 3:
+        movex = -3
+        movey = 0
+    elif selectarea == 4:
+        movex = 0
+        movey = 0
+    elif selectarea == 5:
+        movex = 3
+        movey = 0
+    elif selectarea == 6:
+        movex = -3
+        movey = 3
+    elif selectarea == 7:
+        movex = 0
+        movey = 3
+    elif selectarea == 8:
+        movex = 3
+        movey = 3
+    inlist = []
+    for row in range(3):
+        for col in range(3):
+            if sudokuboard[3+movey+row][3+movex+col] != ' ':
+                inlist.append(sudokuboard[3+movey+row][3+movex+col])
+    outlist = []
+    for i in range(1,10,1):
+        if i not in inlist:
+            outlist.append(i)
+    return outlist
+# #테스트용
+# for i in range(9):
+#     print(checkarea(i))
+# for i in range(9):
+#     print(checkarea(i))
+# for i in range(9):
+#     print(checkarea(i))
